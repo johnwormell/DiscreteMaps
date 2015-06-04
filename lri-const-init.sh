8943f565a51a208b7925fb78b9dbb8370a82d4c6
John Peter Wormell <wormellj@maths.usyd.edu.au>
2015-06-04 18:13:53 +1000

    Minor updates to lrd etc. New results Some random log files etc I hope to remove

--- lri-const-init.sh
+++ lri-const-init.sh
@@ -1,4 +1,4 @@
 ls | grep log- | xargs rm
-bat log-L1c julia -p 1 lri.jl L1 40000 40000 const
-bat log-D1c julia -p 1 lri.jl D1 40000 40000 const
-bat log-W3c julia -p 2 lri.jl W3 40000 40000 const
+bat log/log-L1c julia -p 1 lri.jl L1 40000 40000 const
+bat log/log-D1c julia -p 1 lri.jl D1 40000 40000 const
+bat log/log-W3c julia -p 2 lri.jl W3 40000 40000 const
