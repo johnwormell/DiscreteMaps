5aecfd116fa95dddf30801837a3e47ff1c0f2d37
John Peter Wormell <wormellj@maths.usyd.edu.au>
2015-06-29 12:24:05 +1000

    Some ld results, minor cleanup
    Added rs results
    Removed lrp results from repository as they take up hella space and
    can be checked using command line
    Deleted log-files in main folder
    Fixed mistake in scripts so log-files go to log folder

--- lri-init.sh
+++ lri-init.sh
@@ -5,5 +5,5 @@ cd ..
 PIS="C2 D2 L2 M1 N1 W3 W3 W3 X3 X3 X3 Y1 Y1 Y1
 "
 for PI in $PIS; do
-  bat log-s$PI julia -p 2 lri.jl $PI 40000000 40000
+  bat log/log-s$PI julia -p 2 lri.jl $PI 40000000 40000
 done
--- lrp-init.sh
+++ lrp-init.sh
@@ -4,5 +4,5 @@ ls | grep log-p | xargs rm
 cd ..
 PIS="C2 D2 L2 M1 W3 W3 X3 X3 Y1 Y1"
 for PI in $PIS; do
-  bat log-p$PI julia -p 1 lri.jl $PI 40000 40000 const
+  bat log/log-p$PI julia -p 1 lri.jl $PI 40000 40000 const
 done
