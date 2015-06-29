952b92a02e7a123a6fce146985a297d63264a6d7
John Peter Wormell <wormellj@maths.usyd.edu.au>
2015-06-29 17:20:11 +1000

    Added lb results

--- lrb.jl
+++ lrb.jl
@@ -1,7 +1,7 @@
 @everywhere setupcode = quote
   include("DiscreteMaps.jl")
   using DiscreteMaps, HDF5, JLD, Dates
-  endtime = DateTime(2015,06,29,10,00,00)
+  endtime = DateTime(2015,06,30,10,00,00)
 
   function peturbsample(M,N,deps,phase)
     DiscreteMaps.timedsample("Lh",endtime=endtime,NP=M,NCycles=1,startstring="results/lrb/rb-$(M)-$(N)-$(deps)-$(round(phase/2pi,3))",
@@ -15,7 +15,7 @@ end
 DiscreteMaps.newpath("results/lrb")
 (length(ARGS) == 1) ? (M = int(ARGS[1])) : (M = 20)
  while (now() < endtime)
-  for N in [40000,60000,80000], deps in ([1:10]*1e-6), phase in (2pi * [0:9]/10)
+  for N in [40000,60000,80000], deps in ([1:10]*1e-6), phase in (2pi * [0:29]/30)
     peturbsample(M,N,deps,phase)
   end
  end
