84a345b55c89310ee64e9e9db39e92bbf2997cc1
John Wormell <jpwormell@gmail.com>
2015-07-08 19:10:33 +1000

    Removed iteration scripts

--- lrd-init.sh
+++
@@ -1,3 +0,0 @@
-rm log/log-lrd
-bat log/log-lrd julia -p 13 lrd.jl C1 C2 D1 L1 M1 N1 X1 X2 X3 W1 W2 W3 Y1
-
--- lri-init4andona.sh
+++
@@ -1,3 +0,0 @@
-bat log/log-X1 julia -p 7 lri.jl X3 40000000 40000
-bat log/log-Y1 julia -p 7 lri.jl Y3 40000000 40000
-bat log/log-W1 julia -p 7 lri.jl W3 40000000 40000
